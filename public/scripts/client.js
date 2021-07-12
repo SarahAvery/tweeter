06ae5f5c6bb7783fd71d911f07b3e3538d1e2893
S Avery <sarahashavery@gmail.com>
2021-07-12 21:03:37 +0000

    add fixed navbar

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,9 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-(function ($) {
-  $(document).ready(function () {
-    // Counter characters left
-    jQuery("time.timeago").format(new Date());
-  });
-})(jQuery);
+// (function ($) {
+//   $(document).ready(function () {
+//     //
+//     $(".timeago").timeago();
+//   });
+// })(jQuery);
