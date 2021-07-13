ff18435a676f3bd87ce96e8256c2fccd7bdda6a0
S Avery <sarahashavery@gmail.com>
2021-07-13 19:36:14 +0000

    error message complete

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,19 +3,28 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+// escape function to prevent cross-site scripting
+function escape(str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
 (function ($) {
   $(document).ready(function () {
-    //
+    $("#error-msg").css("display", "none");
 
     const renderTweets = (tweets) => {
       for (const tweet of tweets) {
         const $tweetCard = createTweetElement(tweet);
-        $(".tweet-container").append($tweetCard);
+        $("#column").append($tweetCard);
       }
     };
 
+    // tweet article
     const createTweetElement = (tweetObj) => {
+      const timepast = timeago.format(tweetObj.created_at);
+      // console.log(timepast);
       const $tweet = $(` <article>
       <div class="padding-wrapper">
         <header class="tweet-header">
@@ -26,13 +35,10 @@
           <p class="handle">${tweetObj.user.handle}</p>
         </header>
         <div class="tweet-body">
-          <p>${tweetObj.content.text}</p>
+          <p>${escape(tweetObj.content.text)}</p>
         </div>
         <footer class="tweet-footer">
-          <p>10 days ago</p>
-          <!-- <time class="timeago" datetime="2019-11-27T09:24:17Z"
-          >November 27, 2019</time
-        > -->
+          <p>${timepast}</p>
           <div class="icons">
             <i class="fas fa-flag fa-xs"></i>
             <i class="fas fa-retweet fa-xs"></i>
@@ -44,50 +50,40 @@
       return $tweet;
     };
 
-    // DATABASE
-
-    const tweetDB = [
-      {
-        user: {
-          name: "Newton",
-          avatars: "https://i.imgur.com/73hZDYK.png",
-          handle: "@SirIsaac"
-        },
-        content: {
-          text: "If I have seen further it is by standing on the shoulders of giants"
-        },
-        created_at: 1625954859205
-      },
-      {
-        user: {
-          name: "Descartes",
-          avatars: "https://i.imgur.com/nlhLi3I.png",
-          handle: "@rd"
-        },
-        content: {
-          text: "Je pense , donc je suis"
-        },
-        created_at: 1626041259205
-      }
-    ];
-
     // submit event
+
     $("#form").submit((e) => {
       e.preventDefault();
+      $("#error-msg").css("display", "none");
 
-      const tweet = $("textarea").val();
-      const data = $(e.target).serialize();
-      // console.log(form.serialize());
-      // console.log($(this));
-      // console.log(tweet);
-      $.ajax({
-        url: e.target.action,
-        type: "POST",
-        data,
-        success: (response) => console.log(response)
-      });
+      const tweetLength = $("textarea").val().length;
+      if (tweetLength >= 1) {
+        const data = $(e.target).serialize();
+
+        $.ajax({
+          url: e.target.action,
+          type: "POST",
+          data,
+          success: (response) => {
+            renderTweets([response]);
+            $("textarea").val("");
+            $(".charLeft").html("140");
+          }
+        });
+      } else {
+        $("#error-msg").css("display", "block");
+        $("#error-msg p").html("You can not send an empty tweet");
+        return false;
+      }
     });
 
-    renderTweets(tweetDB);
+    // load tweets
+    $(function () {
+      $.ajax({
+        url: "http://localhost:8080/tweets",
+        type: "GET",
+        success: (response) => renderTweets(response)
+      });
+    });
   });
 })(jQuery);
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,13 @@
 (function ($) {
   $(document).ready(function () {
-    // Counter characters left
-    $("#tweet-text").on("input", function () {
-      const maxLength = $(this).attr("maxLength");
-      const currLength = $(this).val().length;
-      const charsLeft = Number(maxLength - currLength);
+    $("#form")
+      .find("#tweet-text")
+      .on("input", function () {
+        const maxLength = $(this).attr("maxLength");
+        const currLength = $(this).val().length;
+        const charsLeft = Number(maxLength - currLength);
 
-      $(".charLeft").html(charsLeft);
-    });
+        $("#form").find(".charLeft").html(charsLeft);
+      });
   });
 })(jQuery);
