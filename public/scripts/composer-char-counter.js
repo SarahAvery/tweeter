d227ade4da5bf46582d88115206712e2ca416a70
S Avery <sarahashavery@gmail.com>
2021-07-14 01:45:43 +0000

    media queries

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,6 @@
 (function ($) {
   $(document).ready(function () {
+    // char counter
     $("#form")
       .find("#tweet-text")
       .on("input", function () {
@@ -9,5 +10,28 @@
 
         $("#form").find(".charLeft").html(charsLeft);
       });
+
+    const animateTopHandler = function () {
+      $("html").animate({ scrollTop: 0 }, "slow");
+    };
+
+    $("#topBtn").on("click", animateTopHandler);
+  });
+
+  $(document).scroll(function () {
+    // scroll button
+    const scrollPosition = $(window).scrollTop();
+    if (scrollPosition > 100) {
+      $("#topBtn").css("visibility", "visible");
+    } else {
+      $("#topBtn").css("visibility", "hidden");
+    }
+
+    // nav
+    if (scrollPosition > 300) {
+      $("body").addClass("scrolled");
+    } else {
+      $("body").removeClass("scrolled");
+    }
   });
 })(jQuery);
